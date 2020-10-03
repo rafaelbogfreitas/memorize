af6e0ea1e0db0bbcb901c530b95b2f9460ee8ebd
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-10-03 08:54:44 -0300

    Add new file MemoryGame to conform to the MVVM desigh pattern

---
+++ memorize/MemoryGame.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGame.swift
+//  memorize
+//
+//  Created by Rafael Freitas on 03/10/20.
+//
+
+import Foundation
+
+struct MemoryGame {
+    
+    var cards: [Card]
+    
+    struct Card {
+        
+    }
+}
