50e96b26ee04e5927eafa2b723cec129273f6c10
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-10-03 10:07:07 -0300

    Create ViewModel file, EmojiMemoryGame

---
+++ memorize/EmojiMemoryGame.swift
@@ -0,0 +1,18 @@
+//
+//  EmojiMemoryGame.swift
+//  memorize
+//
+//  Created by Rafael Freitas on 03/10/20.
+//
+
+import SwiftUI
+
+//MARK: - ViewModel
+
+/*
+    The ViewModel file is essencially a "portal" between the view and the model
+*/
+
+class EmojiMemoryGame {
+        
+}
